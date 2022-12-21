6cfac34a3c2e8b77c3ddd4988de3873293ced1ed
abreggio <abreggio@gmail.com>
2022-12-21 17:02:50 +0100

    added license header

--- lib/plot_avg_c3_cz_c4.m
+++ lib/plot_avg_c3_cz_c4.m
@@ -1,3 +1,9 @@
+%      This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
 function plot_avg_c3_cz_c4(trials_matrix_to_plot)
     
     % honestly couldn't figure out the map to the actual electrodes, just

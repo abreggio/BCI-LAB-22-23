209031058701c564da86573494eceaa5962839fc
Gianmarco Guarnier <49094051+gianmarcoguarnier@users.noreply.github.com>
2023-02-02 13:52:34 +0100

    Add files via upload

--- lib/GDF_event_decorator.m
+++ lib/GDF_event_decorator.m
@@ -7,126 +7,78 @@
 classdef GDF_event_decorator
     properties
         event_header
-        file_size
     end
-
     methods
-
-        % init 
-        function this = GDF_event_decorator(header)
-            this.event_header = header.EVENT;
-            this.file_size = header.FILE.POS;
+        
+        function this = GDF_event_decorator(event_header)
+                this.event_header = event_header;
         end
+        
 
-        % concatenate different headers with '+' operation
-        function new = plus(this,other)
+        function new = plus(this, other)
             if this.event_header.SampleRate ~= other.event_header.SampleRate
                 ME = MException('different samplerate', 'cant add headers with different sample rate');
                 throw(ME)
             end
-            header.EVENT.POS = [];
-            header.EVENT.TYP = [];
-            header.EVENT.DUR = [];
-            header.EVENT.CHN = [];
-            header.EVENT.SampleRate = other.event_header.SampleRate;
-            header.EVENT.POS = cat(1, this.event_header.POS, other.event_header.POS + this.file_size);
-            header.EVENT.TYP = cat(1, this.event_header.TYP, other.event_header.TYP);
-            header.EVENT.DUR = cat(1, this.event_header.DUR, other.event_header.DUR);
-            header.EVENT.CHN = cat(1, this.event_header.CHN, other.event_header.CHN);
-            header.FILE.POS = this.file_size + other.file_size;
+            header.POS = [];
+            header.TYP = [];
+            header.DUR = [];
+            header.CHN = [];
+            header.SampleRate = other.event_header.SampleRate;
+            header.POS = cat(1, this.event_header.POS, other.event_header.POS + this.event_header.FILE.SIZE);
+            header.TYP = cat(1, this.event_header.TYP, other.event_header.TYP);
+            header.DUR = cat(1, this.event_header.DUR, other.event_header.DUR);
+            header.CHN = cat(1, this.event_header.CHN, other.event_header.CHN);
             new = GDF_event_decorator(header);
         end
-
-        % FUNDAMENTALS FUCNTIONS
-
-        % return Sample Rate
-        function SR = get_SR(obj)
-            SR = obj.event_header.SampleRate;
-        end
-
-        % return POS
-        function POS = get_pos(obj)
-            POS = obj.event_header.POS;
-        end
-        
-        % return DUR
-        function DUR = get_dur(obj)
-            DUR = obj.event_header.DUR;
-        end
-
-        % return TYP
-        function TYP = get_typ(obj)
-            TYP = obj.event_header.TYP;
-        end
-
-        % return indeces of specified event
-        function r = index_event(obj, event)     
-            r = find(obj.event_header.TYP == event);
-        end
-
-        % return POS of TYP == event
         function r = get_pos_for(obj, event_type)
-            r= obj.event_header.POS(obj.event_header.TYP == event_type);
+            r = obj.event_header.POS(obj.event_header.TYP == event_type);
         end
 
-        % return DUR of TYP == event
-        function r=get_dur_for(obj, event_type)
-            r= obj.event_header.DUR(obj.event_header.TYP == event_type);
-        end
-
-        % return TYP of TYP == [<events>] 
-        function r = get_typ_for(obj, events)
-            flag = zeros(length(obj.event_header.TYP),1,'logical');
-            for i=1:length(events)
-                event = events(i);
-                flag = flag|obj.event_header.TYP == event;
-            end
-            r = obj.event_header.TYP(flag); 
-        end
-        
-
-        % UTILITY FUNCTIONS
-
-        % get position 
-        function r = get_pos_for_start(obj)
-            r = obj.get_pos_for(1);
-        end
-
-        % get positions and labels of CUES : 771 (both feet) - 773 (both hands) - 783 (rest)
-        function [CuePOS,CueTYP] = get_cues(obj)
-            % + r [2 x NumCues]
-            % |- r{1} : position of every cues
-            % |- r{2} : cues ID 
-            % (duration not informative)
-
-            CuePOS= obj.event_header.POS(obj.event_header.TYP == 771 | obj.event_header.TYP == 773 | obj.event_header.TYP == 783);
-            CueTYP= obj.get_typ_for([771,773,783]);
-        end
-        
-        % get positions of continuous feedback start
-        function r = get_pos_for_continuous_feedback(obj)
-            r = obj.get_pos_for(781);
-        end
-
-        % get duration of continuous feedback
-        function r = get_dur_for_continuous_feedback(obj)
-            r = obj.get_dur_for(781);
-        end
-        
-        % get position for hit/miss occurances
-        function r = get_pos_for_hit(obj)
-            r = obj.get_pos_for(897);
-        end
-
-        % get position for cross apprearances
         function r = get_pos_for_fixation(obj)
             r = obj.get_pos_for(786);
         end
-
-        % get duration for cross fixation
-        function r = get_dur_for_fixation(obj)
-            r = obj.get_dur_for(786);
+        function r = get_pos_for_cue(obj)
+            r = obj.event_header.POS(obj.event_header.TYP == 771 | obj.event_header.TYP == 773 | obj.event_header.TYP == 783);
+        end
+        function r = get_pos_for_continuous_feedback(obj)
+            r = obj.get_pos_for(781);
+        end
+        function r = get_pos_for_start(obj)
+            r = obj.get_pos_for(1);
+        end
+        function r = get_pos_for_hit(obj)
+            r = obj.get_pos_for(897);
+        end
+        function r = get_dur(obj)
+            r = obj.event_header.DUR;
         end
 
-    end % methods
-end %class
+        function r = get_dur_for(obj,event_type)
+            r= obj.event_header.DUR(obj.event_header.TYP == event_type);
+        end
+
+        function r = get_dur_for_continuous_feedback(obj)
+            r= obj.get_dur_for(781);
+        end
+
+        function r = get_dur_for_cue(obj)
+            r = cat(1, obj.get_dur_for(773), obj.get_dur_for(771));
+        end
+        
+        function r = get_typ(obj)
+            r = obj.event_header.TYP;
+        end
+        function r = get_pos(obj)
+            r = obj.event_header.POS;
+        end
+        function r = get_chn(obj)
+            r = obj.event_header.CHN;
+        end
+        function r = index_event(obj, event)     
+            r = obj.event_header.TYP == event;
+        end
+
+     
+    end
+end
